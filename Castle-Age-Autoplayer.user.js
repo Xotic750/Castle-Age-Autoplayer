caca1044300f06bbed4227d72f59d3e0767f271d
Xotic750 <xotic750@gmail.com>
2010-03-10 11:50:29 +0100

    Updated version number v138.74

--- Castle-Age-Autoplayer.user.js
+++ Castle-Age-Autoplayer.user.js
@@ -2,7 +2,7 @@
 // @name           Castle Age Autoplayer
 // @namespace      caap
 // @description    Auto player for Castle Age
-// @version        138.73
+// @version        138.74
 // @require        http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js
 // @include        http*://apps.*facebook.com/castle_age/*
 // @include        http://www.facebook.com/common/error.html
@@ -13,7 +13,7 @@
 // @compatability  Firefox 3.0+, Chrome 4+, Flock 2.0+
 // ==/UserScript==
 
-var thisVersion = "138.73";
+var thisVersion = "138.74";
 
 var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') != -1 ? true : false;
 var isnot_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') == -1  ? true : false;
@@ -4308,7 +4308,7 @@ clearLinks: function (resetall){
 		gm.setValue("waitMonsterLoad",0);
 		gm.setValue("urlixc","~");
 	}
-	
+
 	gm.setValue("urlix","~");
 	gm.setValue('doaid', '~');
 	gm.setValue('legio', '~');
