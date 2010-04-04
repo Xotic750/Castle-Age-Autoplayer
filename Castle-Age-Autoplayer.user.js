9520bdd38d51034c2d00b1534f437ceec09a61e0
Artifice <artifice@c-loomis.com>
2010-04-05 06:56:56 +0900

    version number

--- Castle-Age-Autoplayer.user.js
+++ Castle-Age-Autoplayer.user.js
@@ -2,7 +2,7 @@
 // @name           Castle Age Autoplayer
 // @namespace      caap
 // @description    Auto player for Castle Age
-// @version        140.14.10
+// @version        140.14.11
 // @require        http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js
 // @include        http*://apps.*facebook.com/castle_age/*
 // @include        http://www.facebook.com/common/error.html
@@ -22,7 +22,7 @@
 ///////////////////////////
 
 var caapGlob = {};
-caapGlob.thisVersion = "140.14.10";
+caapGlob.thisVersion = "140.14.11";
 caapGlob.gameName = 'castle_age';
 caapGlob.SUC_script_num = 57917;
 caapGlob.discussionURL = 'http://senses.ws/caap/index.php';
