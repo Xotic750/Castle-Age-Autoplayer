9e41a07fd9d00858135733406e8c8920afe6adea
Xotic750 <Xotic750@gmail.com>
2011-03-23 09:02:12 +0100

    Prep IE release loader

--- IE/Castle-Age-Autoplayer-Loader.ieuser.js
+++ IE/Castle-Age-Autoplayer-Loader.ieuser.js
@@ -15,7 +15,7 @@
 (function () {
     var a = document.createElement('script');
     a.type = 'text/javascript';
-    a.src = 'http://castle-age-auto-player.googlecode.com/files/Castle-Age-Autoplayer.ieuser.js';
+    a.src = 'http://castle-age-auto-player.googlecode.com/files/Castle-Age-Autoplayer-IE.ieuser.js';
     (document.head || document.getElementsByTagName('head')[0]).appendChild(a);
     (document.head || document.getElementsByTagName('head')[0]).removeChild(a);
 }());
