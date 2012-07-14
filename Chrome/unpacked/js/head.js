97618878f13591af8bfe5aed6004ad5ebc9078d4
magowiz <magowiz@gmail.com>
2012-07-14 08:33:15 +0200

    d65 : Kessaran, better Id finder

--- Chrome/unpacked/js/head.js
+++ Chrome/unpacked/js/head.js
@@ -3,7 +3,7 @@
 // @namespace      caap
 // @description    Auto player for Castle Age
 // @version        141.0.0
-// @dev            64
+// @dev            65
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
@@ -20,7 +20,7 @@ var caapjQuery = "1.7.1";
 //////////////////////////////////
 
 var	caapVersion = "141.0.0",
-		devVersion = "64",
+		devVersion = "65",
 		hiddenVar = true,
 		caap_timeout = 0,
 		image64 = {},
