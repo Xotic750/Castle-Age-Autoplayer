b5e99c94dc23ae2d91537adeeaf8ca024b09d604
magowiz <magowiz@gmail.com>
2012-11-06 00:59:04 +0100

    d87 : integrate a fix for auto-feat feature from Jigoku

--- Chrome/unpacked/js/head.js
+++ Chrome/unpacked/js/head.js
@@ -3,7 +3,7 @@
 // @namespace      caap
 // @description    Auto player for Castle Age
 // @version        141.0.0
-// @dev            86
+// @dev            87
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
@@ -20,7 +20,7 @@ var caapjQuery = "1.7.1";
 //////////////////////////////////
 
 var	caapVersion = "141.0.0",
-		devVersion = "86",
+		devVersion = "87",
 		hiddenVar = true,
 		caap_timeout = 0,
 		image64 = {},
