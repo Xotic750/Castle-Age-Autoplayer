981a39754cdc16cd25f4ba93aca64c0037bfd017
magowiz <magowiz@gmail.com>
2012-11-30 12:08:16 +0100

    :d105: Added Poseidon thanks to Akira181

--- Chrome/unpacked/js/monster.js
+++ Chrome/unpacked/js/monster.js
@@ -1383,6 +1383,37 @@
 
 		},
 
+		"Poseidon" : {
+               alpha        : true,
+                duration     : 168,
+                hp           : 650000000,
+                ach          : 5200000,
+                siege        : 10,
+                siegeClicks  : [15, 30, 45, 60, 75, 100, 150, 200, 250, 300],
+                siegeDam     : [9000000, 11000000, 15000000, 19000000, 23000000, 27000000, 31000000, 35000000, 39000000, 44000000],
+                siege_img    : [
+                    '/graphics/earth_siege_small',
+                    '/graphics/castle_siege_small',
+                    '/graphics/skaar_siege_small',
+                    '/graphics/death_siege_small'
+                ],
+                fort         : true,
+                staUse       : 10,
+                staLvl       : [0, 100, 200, 500],
+                staMax       : [10, 20, 50, 100, 200],
+                nrgMax       : [20, 40, 100, 200, 200],
+                defense_img  : 'nm_green.jpg',
+                levels       : [1,  50, 100, 150],
+                join         : [30, 30, 35,  50],
+                mClass       : 'Epic Boss',
+                mpool        : 1,
+                newbg_img    : ['monster_header_poseidon.jpg'],
+                list_img     : ['monster_poseidon_list.jpg'],
+                cta_img      : ['cta_poseidon.gif'],
+                bodyparts    : 3,
+                partOrder    : [3,2,1]
+            },
+
 
 		"Vorak" : {
                alpha        : true,
