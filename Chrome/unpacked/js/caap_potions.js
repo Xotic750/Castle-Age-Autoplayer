b70cccc7245ec9f401c4505164d563ce44604697
Bonbons <tapuna@hotmail.com>
2012-12-14 10:30:29 -1000

    Update Chrome/unpacked/js/caap_potions.js
    Add enable archive bonuses

--- Chrome/unpacked/js/caap_potions.js
+++ Chrome/unpacked/js/caap_potions.js
@@ -68,3 +68,60 @@ caap.autoPotions = function() {
 	}
 };
 /*jslint sub: false */
+
+
+caap.autoArchives = function() {
+	try {
+		var button,hours=24,minutes=0,archiveDIV;
+		
+		if(!config.getItem('AutoArchives', true) || !schedule.check('AutoArchiveTimerDelay')) {
+			caap.setDivContent('archive_mess', schedule.check('AutoArchiveTimerDelay') ? 'Archive = none' : 'Next Archive: ' + $u.setContent(caap.displayTime('AutoArchiveTimerDelay'), "Unknown"));
+			return false;
+		}
+		
+		archiveDIV=$j("div[style*='archive_top']");
+		if(!archiveDIV || archiveDIV.length ===0) {
+			con.log(2, "Going to Item archives for bonuses");
+			if(caap.navigateTo('item_archive_bonus')) {
+				return true;
+			} else {
+				throw "Impossible to navigate to Item archives page";
+			}
+		}
+
+		button = caap.checkForImage('archive_btn_enable.gif');
+		if(button) {
+			caap.click(button);
+		} else {
+			var timespan;
+			timespan=$j("span[style='']");
+			if (timespan) {
+				var timestr,convert1 = new RegExp('([0-9]+)hrs([0-9]+)m', 'i'),timeresult;
+				timestr=timespan.innerText;
+				timeresult=convert1.exec(timestr);
+				if (timeresult) {
+					hours=Math.max(timeresult[1],0);
+					minutes=Math.max(timeresult[2],0);
+				} else {
+					var convert2 = new RegExp('([0-9]+)m', 'i');
+					timestr=timespan.innerText;
+					timeresult=convert2.exec(timestr);
+					if (timeresult) {
+						hours=0
+						minutes=Math.max(timeresult[1],0);
+					} else {
+						con.warn("Could not find timer; so setting to default");
+					}
+				}					
+			} else {
+				con.warn("Could not find timer; so setting to default");
+			}
+		}	
+		schedule.setItem('AutoArchiveTimerDelay', ((hours * 60) + minutes) * 60, 300);	
+		return false;
+		
+	} catch (err) {
+		con.error("ERROR in autoArchives: " + err);
+		return false;
+	}
+};
