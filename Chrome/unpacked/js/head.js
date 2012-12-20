ce233b455fa875b351e102642c6be913b348e54d
magowiz <magowiz@gmail.com>
2012-12-21 00:16:02 +0100

    d118 : archive fix by Jigoku

--- Chrome/unpacked/js/head.js
+++ Chrome/unpacked/js/head.js
@@ -3,7 +3,7 @@
 // @namespace      caap
 // @description    Auto player for Castle Age
 // @version        141.0.0
-// @dev            117
+// @dev            118
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
@@ -22,7 +22,7 @@ var caapjQueryDataTables = "1.9.4";
 //////////////////////////////////
 
 var	caapVersion = "141.0.0",
-		devVersion = "117",
+		devVersion = "118",
 		hiddenVar = true,
 		caap_timeout = 0,
 		image64 = {},
