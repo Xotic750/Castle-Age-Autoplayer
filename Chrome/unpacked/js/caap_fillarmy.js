4f481c971605a1af4b666b16809e3eb549a10175
Graham Fairweather <xotic750@gmail.com>
2013-01-02 18:30:17 +0100

    Beautified code, ran through jslint and corrected code to latest JS standards.

--- Chrome/unpacked/js/caap_fillarmy.js
+++ Chrome/unpacked/js/caap_fillarmy.js
@@ -1,102 +1,118 @@
+/*jslint white: true, browser: true, devel: true, undef: true,
+nomen: true, bitwise: true, plusplus: true,
+regexp: true, eqeq: true, newcap: true */
+/*global window,escape,jQuery,$j,rison,utility,
+$u,chrome,CAAP_SCOPE_RUN,self,caap,config,con,
+schedule,gifting,state,army, general */
+/*jslint maxlen: 256 */
+
 /////////////////////////////////////////////////////////////////////
 //                              FILL ARMY
 /////////////////////////////////////////////////////////////////////
 
-caap.addFriendSpamCheck = 0;
+(function () {
+    "use strict";
 
-caap.autoFillArmy = function() {
-	try {
-		if(!state.getItem('FillArmy', false) || !config.getItem("EnableArmy", true) || caap.domain.which === 0 || caap.domain.which === 1 || caap.domain.which === 4) {
-			return false;
+    caap.addFriendSpamCheck = 0;
+
+    caap.autoFillArmy = function () {
+
+	function addFriend(id) {
+	    function responseCallback(data, textStatus, XMLHttpRequest) {
+		if (caap.addFriendSpamCheck > 0) {
+		    caap.addFriendSpamCheck -= 1;
 		}
 
-		function addFriend(id) {
-			try {
-				function responseCallback(data, textStatus, XMLHttpRequest) {
-					if(caap.addFriendSpamCheck > 0) {
-						caap.addFriendSpamCheck -= 1;
-					}
+		con.log(1, "AddFriend(" + id + "): ", [data, textStatus, XMLHttpRequest]);
+	    }
 
-					con.log(1, "AddFriend(" + id + "): ", textStatus);
-				}
-
-
-				caap.ajax('party.php?twt=jneg&jneg=true&user=' + id + '&lka=' + id + '&etw=9&ref=nf', null, responseCallback, responseCallback);
-				//caap.ajax('index.php?tp=cht&buf=1&lka=' + id, null, responseCallback, responseCallback);
-				return true;
-			} catch (err) {
-				con.error("ERROR in addFriend(" + id + "): " + err);
-				return false;
-			}
-		}
-
-		var armyCount = state.getItem("ArmyCount", 0), fillArmyList = state.getItem('FillArmyList', []), batchCount = 5, i = 0;
-
-		con.log(2, "fillArmyList:", fillArmyList);
-		con.log(2, "armyCount:", armyCount);
-		if(armyCount === 0) {
-			fillArmyList = state.setItem('FillArmyList', army.getDiffList());
-			if($u.hasContent(fillArmyList)) {
-				caap.setDivContent('idle_mess', 'Filling Army');
-				con.log(1, "Filling army");
-			} else {
-				caap.setDivContent('idle_mess', '<span style="font-weight: bold;">Fill Army Completed</span>');
-				con.log(1, "Fill Army Completed: no friends found");
-				window.setTimeout(function() {
-					caap.setDivContent('idle_mess', '');
-				}, 5000);
-
-				state.setItem('FillArmy', false);
-				state.setItem("ArmyCount", 0);
-				state.setItem('FillArmyList', []);
-				return false;
-			}
-		}
-
-		if($u.hasContent(fillArmyList)) {
-			con.log(2, "inside");
-			// Add army members //
-			if(fillArmyList.length < 5) {
-				batchCount = fillArmyList.length;
-			} else if(fillArmyList.length - armyCount < 5) {
-				batchCount = fillArmyList.length - armyCount;
-			}
-			batchCount = batchCount - caap.addFriendSpamCheck;
-			for( i = 0; i < batchCount; i += 1) {
-				addFriend(fillArmyList[armyCount]);
-				armyCount += 1;
-				caap.addFriendSpamCheck += 1;
-			}
-
-			caap.setDivContent('idle_mess', 'Filling Army, Please wait...' + armyCount + "/" + fillArmyList.length);
-			con.log(1, 'Filling Army, Please wait...' + armyCount + "/" + fillArmyList.length);
-			state.setItem("ArmyCount", armyCount);
-			if(armyCount >= fillArmyList.length) {
-				caap.setDivContent('idle_mess', '<span style="font-weight: bold;">Fill Army Completed</span>');
-				window.setTimeout(function() {
-					caap.setDivContent('idle_mess', '');
-				}, 5000);
-
-				con.log(1, "Fill Army Completed");
-				state.setItem('FillArmy', false);
-				state.setItem("ArmyCount", 0);
-				state.setItem('FillArmyList', []);
-				return false;
-			}
-		}
-
-		con.log(2, "At end returning true");
+	    try {
+		caap.ajax('party.php?twt=jneg&jneg=true&user=' + id + '&lka=' + id + '&etw=9&ref=nf', null, responseCallback, responseCallback);
 		return true;
-	} catch (err) {
-		con.error("ERROR in autoFillArmy: " + err);
-		caap.setDivContent('idle_mess', '<span style="font-weight: bold;">Fill Army Failed</span>');
-		window.setTimeout(function() {
-			caap.setDivContent('idle_mess', '');
-		}, 5000);
-
-		state.setItem('FillArmy', false);
-		state.setItem("ArmyCount", 0);
-		state.setItem('FillArmyList', []);
+	    } catch (err) {
+		con.error("ERROR in addFriend(" + id + "): " + err);
 		return false;
+	    }
 	}
-};
+
+        try {
+            if (!state.getItem('FillArmy', false) || !config.getItem("EnableArmy", true) || caap.domain.which === 0 || caap.domain.which === 1 || caap.domain.which === 4) {
+                return false;
+            }
+
+            var armyCount = state.getItem("ArmyCount", 0),
+                fillArmyList = state.getItem('FillArmyList', []),
+                batchCount = 5,
+                i = 0;
+
+            con.log(2, "fillArmyList:", fillArmyList);
+            con.log(2, "armyCount:", armyCount);
+            if (armyCount === 0) {
+                fillArmyList = state.setItem('FillArmyList', army.getDiffList());
+                if ($u.hasContent(fillArmyList)) {
+                    caap.setDivContent('idle_mess', 'Filling Army');
+                    con.log(1, "Filling army");
+                } else {
+                    caap.setDivContent('idle_mess', '<span style="font-weight: bold;">Fill Army Completed</span>');
+                    con.log(1, "Fill Army Completed: no friends found");
+                    window.setTimeout(function () {
+                        caap.setDivContent('idle_mess', '');
+                    }, 5000);
+
+                    state.setItem('FillArmy', false);
+                    state.setItem("ArmyCount", 0);
+                    state.setItem('FillArmyList', []);
+                    return false;
+                }
+            }
+
+            if ($u.hasContent(fillArmyList)) {
+                con.log(2, "inside");
+                // Add army members //
+                if (fillArmyList.length < 5) {
+                    batchCount = fillArmyList.length;
+                } else if (fillArmyList.length - armyCount < 5) {
+                    batchCount = fillArmyList.length - armyCount;
+                }
+
+                batchCount = batchCount - caap.addFriendSpamCheck;
+                for (i = 0; i < batchCount; i += 1) {
+                    addFriend(fillArmyList[armyCount]);
+                    armyCount += 1;
+                    caap.addFriendSpamCheck += 1;
+                }
+
+                caap.setDivContent('idle_mess', 'Filling Army, Please wait...' + armyCount + "/" + fillArmyList.length);
+                con.log(1, 'Filling Army, Please wait...' + armyCount + "/" + fillArmyList.length);
+                state.setItem("ArmyCount", armyCount);
+                if (armyCount >= fillArmyList.length) {
+                    caap.setDivContent('idle_mess', '<span style="font-weight: bold;">Fill Army Completed</span>');
+                    window.setTimeout(function () {
+                        caap.setDivContent('idle_mess', '');
+                    }, 5000);
+
+                    con.log(1, "Fill Army Completed");
+                    state.setItem('FillArmy', false);
+                    state.setItem("ArmyCount", 0);
+                    state.setItem('FillArmyList', []);
+                    return false;
+                }
+            }
+
+            con.log(2, "At end returning true");
+            return true;
+        } catch (err) {
+            con.error("ERROR in autoFillArmy: " + err);
+            caap.setDivContent('idle_mess', '<span style="font-weight: bold;">Fill Army Failed</span>');
+            window.setTimeout(function () {
+                caap.setDivContent('idle_mess', '');
+            }, 5000);
+
+            state.setItem('FillArmy', false);
+            state.setItem("ArmyCount", 0);
+            state.setItem('FillArmyList', []);
+            return false;
+        }
+    };
+
+}());
--- Chrome/unpacked/js/caap_heal.js
+++ Chrome/unpacked/js/caap_heal.js
@@ -1,49 +1,66 @@
+/*jslint white: true, browser: true, devel: true, undef: true,
+nomen: true, bitwise: true, plusplus: true,
+regexp: true, eqeq: true, newcap: true */
+/*global window,escape,jQuery,$j,rison,utility,
+$u,chrome,CAAP_SCOPE_RUN,self,caap,config,con,
+schedule,gifting,state,army, general */
+/*jslint maxlen: 256 */
+
 /////////////////////////////////////////////////////////////////////
 //                          HEAL
 /////////////////////////////////////////////////////////////////////
 
-caap.heal = function() {
-	try {
-		var minToHeal = 0, minStamToHeal = 0;
+(function () {
+    "use strict";
 
-		caap.setDivContent('heal_mess', '');
-		minToHeal = config.getItem('MinToHeal', 0);
-		if(minToHeal === "" || minToHeal < 0 || !$u.isNumber(minToHeal)) {
-			return false;
-		}
-		minStamToHeal = config.getItem('MinStamToHeal', 0);
-		if(minStamToHeal === "" || minStamToHeal < 0 || !$u.isNumber(minStamToHeal)) {
-			minStamToHeal = 0;
-		}
+    /* This section is formatted to allow Advanced Optimisation by the Closure Compiler */
+    /*jslint sub: true */
+    caap.heal = function () {
+        try {
+            var minToHeal = 0,
+                minStamToHeal = 0;
 
-		if(!caap.stats['health'] || $j.isEmptyObject(caap.stats['health']) || $j.isEmptyObject(caap.stats['healthT'])) {
-			return false;
-		}
+            caap.setDivContent('heal_mess', '');
+            minToHeal = config.getItem('MinToHeal', 0);
+            if (minToHeal === "" || minToHeal < 0 || !$u.isNumber(minToHeal)) {
+                return false;
+            }
+            minStamToHeal = config.getItem('MinStamToHeal', 0);
+            if (minStamToHeal === "" || minStamToHeal < 0 || !$u.isNumber(minStamToHeal)) {
+                minStamToHeal = 0;
+            }
 
-		if(!caap.stats['stamina'] || $j.isEmptyObject(caap.stats['stamina']) || $j.isEmptyObject(caap.stats['staminaT'])) {
-			return false;
-		}
+            if (!caap.stats['health'] || $j.isEmptyObject(caap.stats['health']) || $j.isEmptyObject(caap.stats['healthT'])) {
+                return false;
+            }
 
-		if((config.getItem('WhenBattle', 'Never') !== 'Never') || (config.getItem('WhenMonster', 'Never') !== 'Never')) {
-			if((caap.inLevelUpMode() || caap.stats['stamina']['num'] >= caap.stats['staminaT']['max']) && caap.stats['health']['num'] < (config.getItem('WhenBattle', 'Never') !== 'Never' && config.getItem('waitSafeHealth', false) ? 13 : 10)) {
-				con.log(1, 'Heal');
-				return caap.navigateTo('keep,keep_healbtn.gif');
-			}
-		}
+            if (!caap.stats['stamina'] || $j.isEmptyObject(caap.stats['stamina']) || $j.isEmptyObject(caap.stats['staminaT'])) {
+                return false;
+            }
 
-		if(caap.stats['health']['num'] >= caap.stats['healthT']['max'] || caap.stats['health']['num'] >= minToHeal) {
-			return false;
-		}
+            if ((config.getItem('WhenBattle', 'Never') !== 'Never') || (config.getItem('WhenMonster', 'Never') !== 'Never')) {
+                if ((caap.inLevelUpMode() || caap.stats['stamina']['num'] >= caap.stats['staminaT']['max']) && caap.stats['health']['num'] < (config.getItem('WhenBattle', 'Never') !== 'Never' && config.getItem('waitSafeHealth', false) ? 13 : 10)) {
+                    con.log(1, 'Heal');
+                    return caap.navigateTo('keep,keep_healbtn.gif');
+                }
+            }
 
-		if(caap.stats['stamina']['num'] < minStamToHeal) {
-			caap.setDivContent('heal_mess', 'Waiting for stamina to heal: ' + caap.stats['stamina']['num'] + '/' + minStamToHeal);
-			return false;
-		}
+            if (caap.stats['health']['num'] >= caap.stats['healthT']['max'] || caap.stats['health']['num'] >= minToHeal) {
+                return false;
+            }
 
-		con.log(1, 'Heal');
-		return caap.navigateTo('keep,keep_healbtn.gif');
-	} catch (err) {
-		con.error("ERROR in heal: " + err);
-		return false;
-	}
-}; 
+            if (caap.stats['stamina']['num'] < minStamToHeal) {
+                caap.setDivContent('heal_mess', 'Waiting for stamina to heal: ' + caap.stats['stamina']['num'] + '/' + minStamToHeal);
+                return false;
+            }
+
+            con.log(1, 'Heal');
+            return caap.navigateTo('keep,keep_healbtn.gif');
+        } catch (err) {
+            con.error("ERROR in heal: " + err);
+            return false;
+        }
+    };
+    /*jslint sub: false */
+
+}());
