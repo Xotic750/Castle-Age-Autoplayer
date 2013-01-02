af135b931441f69425edfac6d6266c691e8d1359
Graham Fairweather <xotic750@gmail.com>
2013-01-02 21:29:09 +0100

    Beautified code, ran through jslint and corrected code to latest JS standards.

--- Chrome/unpacked/js/caap_bank.js
+++ Chrome/unpacked/js/caap_bank.js
@@ -1,82 +1,109 @@
+/*jslint white: true, browser: true, devel: true, undef: true,
+nomen: true, bitwise: true, plusplus: true,
+regexp: true, eqeq: true, newcap: true, forin: false */
+/*global window,escape,jQuery,$j,rison,utility,
+$u,chrome,CAAP_SCOPE_RUN,self,caap,config,con,
+schedule,gifting,state,army, general,session,monster,guild_monster */
+/*jslint maxlen: 256 */
+
 /////////////////////////////////////////////////////////////////////
 //                          BANKING
-// Keep it safe!
+//                       Keep it safe!
 /////////////////////////////////////////////////////////////////////
 
-caap.immediateBanking = function() {
-	if(!config.getItem("BankImmed", false)) {
-		return false;
-	}
+(function () {
+    "use strict";
 
-	return caap.bank();
-};
-/* This section is formatted to allow Advanced Optimisation by the Closure Compiler */
-/*jslint sub: true */
-caap.bank = function() {
-	try {
-		if(config.getItem("NoBankAfterLvl", true) && state.getItem('KeepLevelUpGeneral', false)) {
-			return false;
-		}
+    caap.immediateBanking = function () {
+        if (!config.getItem("BankImmed", false)) {
+            return false;
+        }
 
-		var maxInCash = -1, minInCash = 0, depositButton = null, numberInput = null, deposit = 0;
-		maxInCash = config.getItem('MaxInCash', -1);
-		minInCash = config.getItem('MinInCash', 0);
-		if(!maxInCash || maxInCash < 0 || caap.stats['gold']['cash'] <= minInCash || caap.stats['gold']['cash'] < maxInCash || caap.stats['gold']['cash'] < 10) {
-			return false;
-		}
+        return caap.bank();
+    };
 
-		if(general.Select('BankingGeneral')) {
-			return true;
-		}
-		depositButton = $j("input[src*='btn_stash.gif']");
-		if(!depositButton || !depositButton.length) {
-			// Cannot find the link
-			return caap.navigateTo('keep');
-		}
-		numberInput = $j("input[name='stash_gold']");
-		if(!numberInput || !numberInput.length) {
-			con.warn('Cannot find box to put in number for bank deposit.');
-			return false;
-		}
-		deposit = caap.stats['gold']['cash'] - minInCash;
-		numberInput.attr("value", deposit);
-		con.log(1, 'Depositing into bank:', deposit);
-		caap.click(depositButton);
-		return true;
-	} catch (err) {
-		con.error("ERROR in Bank: " + err);
-		return false;
-	}
-};
-caap.retrieveFromBank = function(num) {
-	try {
-		if(num <= 0) {
-			return false;
-		}
+    /* This section is formatted to allow Advanced Optimisation by the Closure Compiler */
+    /*jslint sub: true */
+    caap.bank = function () {
+        try {
+            if (config.getItem("NoBankAfterLvl", true) && state.getItem('KeepLevelUpGeneral', false)) {
+                return false;
+            }
 
-		var retrieveButton = null, numberInput = null, minInStore = 0;
-		retrieveButton = $j("input[src*='keep_btn_retireve.gif']");
-		if(!retrieveButton || !retrieveButton.length) {
-			// Cannot find the link
-			return caap.navigateTo('keep');
-		}
-		minInStore = config.getItem('minInStore', 0);
-		if(!(minInStore || minInStore <= caap.stats['gold']['bank'] - num)) {
-			return false;
-		}
-		numberInput = $j("input[name='get_gold']");
-		if(!numberInput || !numberInput.length) {
-			con.warn('Cannot find box to put in number for bank retrieve.');
-			return false;
-		}
+            var maxInCash = -1,
+                minInCash = 0,
+                depositButton = null,
+                numberInput = null,
+                deposit = 0;
+            maxInCash = config.getItem('MaxInCash', -1);
+            minInCash = config.getItem('MinInCash', 0);
+            if (!maxInCash || maxInCash < 0 || caap.stats['gold']['cash'] <= minInCash || caap.stats['gold']['cash'] < maxInCash || caap.stats['gold']['cash'] < 10) {
+                return false;
+            }
 
-		numberInput.attr("value", num);
-		con.log(1, 'Retrieving from bank:', num);
-		state.setItem('storeRetrieve', '');
-		caap.click(retrieveButton);
-		return true;
-	} catch (err) {
-		con.error("ERROR in retrieveFromBank: " + err);
-		return false;
-	}
-};
+            if (general.Select('BankingGeneral')) {
+                return true;
+            }
+
+            depositButton = $j("input[src*='btn_stash.gif']");
+            if (!depositButton || !depositButton.length) {
+                // Cannot find the link
+                return caap.navigateTo('keep');
+            }
+
+            numberInput = $j("input[name='stash_gold']");
+            if (!numberInput || !numberInput.length) {
+                con.warn('Cannot find box to put in number for bank deposit.');
+                return false;
+            }
+
+            deposit = caap.stats['gold']['cash'] - minInCash;
+            numberInput.attr("value", deposit);
+            con.log(1, 'Depositing into bank:', deposit);
+            caap.click(depositButton);
+            return true;
+        } catch (err) {
+            con.error("ERROR in Bank: " + err);
+            return false;
+        }
+    };
+
+    caap.retrieveFromBank = function (num) {
+        try {
+            if (num <= 0) {
+                return false;
+            }
+
+            var retrieveButton = null,
+                numberInput = null,
+                minInStore = 0;
+
+            retrieveButton = $j("input[src*='keep_btn_retireve.gif']");
+            if (!retrieveButton || !retrieveButton.length) {
+                // Cannot find the link
+                return caap.navigateTo('keep');
+            }
+
+            minInStore = config.getItem('minInStore', 0);
+            if (!(minInStore || minInStore <= caap.stats['gold']['bank'] - num)) {
+                return false;
+            }
+
+            numberInput = $j("input[name='get_gold']");
+            if (!numberInput || !numberInput.length) {
+                con.warn('Cannot find box to put in number for bank retrieve.');
+                return false;
+            }
+
+            numberInput.attr("value", num);
+            con.log(1, 'Retrieving from bank:', num);
+            state.setItem('storeRetrieve', '');
+            caap.click(retrieveButton);
+            return true;
+        } catch (err) {
+            con.error("ERROR in retrieveFromBank: " + err);
+            return false;
+        }
+    };
+
+}());
