2157bf0fd9281b97d521265a2dff35321d358bed
Graham Fairweather <xotic750@gmail.com>
2013-01-02 17:36:03 +0100

    Beautified code, ran through jslint and corrected code to latest JS standards.

--- Chrome/unpacked/js/caap_autostat.js
+++ Chrome/unpacked/js/caap_autostat.js
@@ -1,231 +1,271 @@
+/*jslint white: true, browser: true, devel: true, undef: true,
+nomen: true, bitwise: true, plusplus: true,
+regexp: true, eqeq: true, newcap: true */
+/*global window,escape,jQuery,$j,rison,utility,
+$u,chrome,CAAP_SCOPE_RUN,self,caap,config,con,
+schedule,gifting,state,army, general */
+/*jslint maxlen: 256 */
+
 /////////////////////////////////////////////////////////////////////
 //                              IMMEDIATEAUTOSTAT
 /////////////////////////////////////////////////////////////////////
 
-caap.immediateAutoStat = function() {
-	if(!config.getItem("StatImmed", false) || !config.getItem('AutoStat', false)) {
-		return false;
-	}
+(function () {
+    "use strict";
 
-	return caap.autoStat();
-};
-////////////////////////////////////////////////////////////////////
-//                      Auto Stat
-////////////////////////////////////////////////////////////////////
+    caap.immediateAutoStat = function () {
+        if (!config.getItem("StatImmed", false) || !config.getItem('AutoStat', false)) {
+            return false;
+        }
 
-/* This section is formatted to allow Advanced Optimisation by the Closure Compiler */
-/*jslint sub: true */
-caap.increaseStat = function(attribute, attrAdjust, atributeSlice) {
-	try {
-		attribute = attribute.toLowerCase();
-		var button = $j(), level = 0, attrCurrent = 0, energy = 0, stamina = 0, attack = 0, defense = 0, health = 0, attrAdjustNew = 0, energyDiv = $j("a[href*='energy_max']", atributeSlice), staminaDiv = $j("a[href*='stamina_max']", atributeSlice), attackDiv = $j("a[href*='attack']", atributeSlice), defenseDiv = $j("a[href*='defense']", atributeSlice), healthDiv = $j("a[href*='health_max']", atributeSlice), logTxt = "";
+        return caap.autoStat();
+    };
 
-		switch (attribute) {
-			case "energy" :
-				button = energyDiv;
-				break;
-			case "stamina" :
-				button = staminaDiv;
-				break;
-			case "attack" :
-				button = attackDiv;
-				break;
-			case "defense" :
-				button = defenseDiv;
-				break;
-			case "health" :
-				button = healthDiv;
-				break;
-			default :
-				throw "Unable to match attribute: " + attribute;
-		}
+    ////////////////////////////////////////////////////////////////////
+    //                      Auto Stat
+    ////////////////////////////////////////////////////////////////////
 
-		if(!$u.hasContent(button)) {
-			con.warn("Unable to locate upgrade button: Fail ", attribute);
-			return "Fail";
-		}
-		attrAdjustNew = attrAdjust;
-		logTxt = attrAdjust;
-		level = caap.stats['level'];
-		function getValue(div) {
-			return $u.setContent($j("div[onmouseout*='hideItemPopup']", div.parent().parent().parent()).text(), '').regex(/(\d+)/);
-		}
+    /* This section is formatted to allow Advanced Optimisation by the Closure Compiler */
+    /*jslint sub: true */
+    caap.increaseStat = function (attribute, attrAdjust, atributeSlice) {
+        function getValue(div) {
+            return $u.setContent($j("div[onmouseout*='hideItemPopup']", div.parent().parent().parent()).text(), '').regex(/(\d+)/);
+        }
 
-		attrCurrent = getValue(button);
-		energy = getValue(energyDiv);
-		stamina = getValue(staminaDiv);
-		if(level >= 10) {
-			attack = getValue(attackDiv);
-			defense = getValue(defenseDiv);
-			health = getValue(healthDiv);
-		}
+        try {
+            attribute = attribute.toLowerCase();
+            var button = $j(),
+                level = 0,
+                attrCurrent = 0,
+                energy = 0,
+                stamina = 0,
+                attack = 0,
+                defense = 0,
+                health = 0,
+                attrAdjustNew = 0,
+                energyDiv = $j("a[href*='energy_max']", atributeSlice),
+                staminaDiv = $j("a[href*='stamina_max']", atributeSlice),
+                attackDiv = $j("a[href*='attack']", atributeSlice),
+                defenseDiv = $j("a[href*='defense']", atributeSlice),
+                healthDiv = $j("a[href*='health_max']", atributeSlice),
+                logTxt = "";
 
-		if(config.getItem('AutoStatAdv', false)) {
-			//Using eval, so user can define formulas on menu, like energy = level + 50
-			/*jslint evil: true */
-			attrAdjustNew = eval(attrAdjust);
-			/*jslint evil: false */
-			logTxt = "(" + attrAdjust + ")=" + attrAdjustNew;
-		}
+            switch (attribute) {
+                case "energy":
+                    button = energyDiv;
+                    break;
+                case "stamina":
+                    button = staminaDiv;
+                    break;
+                case "attack":
+                    button = attackDiv;
+                    break;
+                case "defense":
+                    button = defenseDiv;
+                    break;
+                case "health":
+                    button = healthDiv;
+                    break;
+                default:
+                    throw "Unable to match attribute: " + attribute;
+            }
 
-		if((attribute === 'stamina') && (caap.stats['points']['skill'] < 2)) {
-			if(attrAdjustNew <= attrCurrent) {
-				con.log(2, "Stamina at requirement: Next");
-				return "Next";
-			} else if(config.getItem("StatSpendAll", false)) {
-				con.log(2, "Stamina requires 2 upgrade points: Next");
-				return "Next";
-			} else {
-				con.log(2, "Stamina requires 2 upgrade points: Save");
-				state.setItem("statsMatch", false);
-				return "Save";
-			}
-		}
+            if (!$u.hasContent(button)) {
+                con.warn("Unable to locate upgrade button: Fail ", attribute);
+                return "Fail";
+            }
 
-		if(attrAdjustNew > attrCurrent) {
-			con.log(2, "Status Before [" + attribute + "=" + attrCurrent + "]  Adjusting To [" + logTxt + "]");
-			caap.click(button);
-			return "Click";
-		}
+            attrAdjustNew = attrAdjust;
+            logTxt = attrAdjust;
+            level = caap.stats['level'];
+            attrCurrent = getValue(button);
+            energy = getValue(energyDiv);
+            stamina = getValue(staminaDiv);
+            if (level >= 10) {
+                attack = getValue(attackDiv);
+                defense = getValue(defenseDiv);
+                health = getValue(healthDiv);
+            }
 
-		return "Next";
-	} catch (err) {
-		con.error("ERROR in increaseStat: " + err);
-		return "Error";
-	}
-};
-caap.autoStatCheck = function() {
-	try {
-		var startAtt = 0, stopAtt = 4, attribute = '', attrValue = 0, n = 0, level = 0, energy = 0, stamina = 0, attack = 0, defense = 0, health = 0, attrAdjust = 0, value = 0, passed = false;
+            if (config.getItem('AutoStatAdv', false)) {
+                //Using eval, so user can define formulas on menu, like energy = level + 50
+                /*jslint evil: true */
+                attrAdjustNew = eval(attrAdjust);
+                /*jslint evil: false */
+                logTxt = "(" + attrAdjust + ")=" + attrAdjustNew;
+            }
 
-		if(!config.getItem('AutoStat', false) || !caap.stats['points']['skill']) {
-			return false;
-		}
+            if ((attribute === 'stamina') && (caap.stats['points']['skill'] < 2)) {
+                if (attrAdjustNew <= attrCurrent) {
+                    con.log(2, "Stamina at requirement: Next");
+                    return "Next";
+                }
 
-		if(config.getItem("AutoStatAdv", false)) {
-			startAtt = 5;
-			stopAtt = 9;
-		}
+                if (config.getItem("StatSpendAll", false)) {
+                    con.log(2, "Stamina requires 2 upgrade points: Next");
+                    return "Next";
+                }
 
-		for( n = startAtt; n <= stopAtt; n += 1) {
-			attribute = config.getItem('Attribute' + n, '').toLowerCase();
-			if(attribute === '') {
-				continue;
-			}
+                con.log(2, "Stamina requires 2 upgrade points: Save");
+                state.setItem("statsMatch", false);
+                return "Save";
+            }
 
-			if(caap.stats['level'] < 10) {
-				if(attribute === 'attack' || attribute === 'defense' || attribute === 'health') {
-					continue;
-				}
-			}
-			attrValue = config.getItem('AttrValue' + n, 0);
-			attrAdjust = attrValue;
-			level = caap.stats['level'];
-			energy = caap.stats['energy']['num'];
-			stamina = caap.stats['stamina']['num'];
-			if(level >= 10) {
-				attack = caap.stats['attack'];
-				defense = caap.stats['defense'];
-				health = caap.stats['health']['num'];
-			}
+            if (attrAdjustNew > attrCurrent) {
+                con.log(2, "Status Before [" + attribute + "=" + attrCurrent + "]  Adjusting To [" + logTxt + "]");
+                caap.click(button);
+                return "Click";
+            }
 
-			if(config.getItem('AutoStatAdv', false)) {
-				//Using eval, so user can define formulas on menu, like energy = level + 50
-				/*jslint evil: true */
-				attrAdjust = eval(attrValue);
-				/*jslint evil: false */
-			}
+            return "Next";
+        } catch (err) {
+            con.error("ERROR in increaseStat: " + err);
+            return "Error";
+        }
+    };
 
-			if(attribute === "attack" || attribute === "defense") {
-				value = caap.stats[attribute];
-			} else {
-				value = caap.stats[attribute]['num'];
-			}
+    caap.autoStatCheck = function () {
+        try {
+            var startAtt = 0,
+                stopAtt = 4,
+                attribute = '',
+                attrValue = 0,
+                n = 0,
+                level = 0,
+                energy = 0,
+                stamina = 0,
+                attack = 0,
+                defense = 0,
+                health = 0,
+                attrAdjust = 0,
+                value = 0,
+                passed = false;
 
-			if(attribute === 'stamina' && caap.stats['points']['skill'] < 2) {
-				if(config.getItem("StatSpendAll", false) && attrAdjust > value) {
-					continue;
-				} else {
-					passed = false;
-					break;
-				}
-			}
+            if (!config.getItem('AutoStat', false) || !caap.stats['points']['skill']) {
+                return false;
+            }
 
-			if(attrAdjust > value) {
-				passed = true;
-				break;
-			}
-		}
+            if (config.getItem("AutoStatAdv", false)) {
+                startAtt = 5;
+                stopAtt = 9;
+            }
 
-		state.setItem("statsMatch", passed);
-		return true;
-	} catch (err) {
-		con.error("ERROR in autoStatCheck: " + err);
-		return false;
-	}
-};
-caap.autoStat = function() {
-	try {
-		if(!config.getItem('AutoStat', false) || !caap.stats['points']['skill']) {
-			return false;
-		}
+            for (n = startAtt; n <= stopAtt; n += 1) {
+                attribute = config.getItem('Attribute' + n, '').toLowerCase();
+                if (attribute !== '') {
+                    level = caap.stats['level'];
 
-		if(!state.getItem("statsMatch", true)) {
-			if(state.getItem("autoStatRuleLog", true)) {
-				con.log(2, "User should possibly change their stats rules");
-				state.setItem("autoStatRuleLog", false);
-			}
+                    if (level >= 10 || (level < 10 && attribute !== 'attack' && attribute !== 'defense' && attribute !== 'health')) {
+                        attrValue = config.getItem('AttrValue' + n, 0);
+                        attrAdjust = attrValue;
+                        energy = caap.stats['energy']['num'];
+                        stamina = caap.stats['stamina']['num'];
+                        attack = caap.stats['attack'];
+                        defense = caap.stats['defense'];
+                        health = caap.stats['health']['num'];
 
-			return false;
-		}
+                        if (config.getItem('AutoStatAdv', false)) {
+                            //Using eval, so user can define formulas on menu, like energy = level + 50
+                            /*jslint evil: true */
+                            attrAdjust = eval(attrValue);
+                            /*jslint evil: false */
+                        }
 
-		var atributeSlice = $j("div[style*='keep_cont_top.jpg']", caap.appBodyDiv), startAtt = 0, stopAtt = 4, attrName = '', attribute = '', attrValue = 0, n = 0, returnIncreaseStat = '';
+                        if (attribute === "attack" || attribute === "defense") {
+                            value = caap.stats[attribute];
+                        } else {
+                            value = caap.stats[attribute]['num'];
+                        }
 
-		if(!$u.hasContent(atributeSlice)) {
-			caap.navigateTo('keep');
-			return true;
-		}
+                        if (attribute === 'stamina' && caap.stats['points']['skill'] < 2 && !config.getItem("StatSpendAll", false) && attrAdjust <= value) {
+                            passed = false;
+                            break;
+                        }
 
-		if(config.getItem("AutoStatAdv", false)) {
-			startAtt = 5;
-			stopAtt = 9;
-		}
+                        if (attrAdjust > value) {
+                            passed = true;
+                            break;
+                        }
+                    }
+                }
+            }
 
-		for( n = startAtt; n <= stopAtt; n += 1) {
-			attrName = 'Attribute' + n;
-			attribute = config.getItem(attrName, '');
-			if(attribute === '') {
-				con.log(4, attrName + " is blank: continue");
-				continue;
-			}
+            state.setItem("statsMatch", passed);
+            return true;
+        } catch (err) {
+            con.error("ERROR in autoStatCheck: " + err);
+            return false;
+        }
+    };
 
-			if(caap.stats['level'] < 10) {
-				if(attribute === 'Attack' || attribute === 'Defense' || attribute === 'Health') {
-					con.log(1, "Characters below level 10 can not increase Attack, Defense or Health: continue");
-					continue;
-				}
-			}
-			attrValue = config.getItem('AttrValue' + n, 0);
-			returnIncreaseStat = caap.increaseStat(attribute, attrValue, atributeSlice);
-			switch (returnIncreaseStat) {
-				case "Next" :
-					con.log(4, attrName + " : next");
-					continue;
-				case "Click" :
-					con.log(4, attrName + " : click");
-					return true;
-				default :
-					con.log(4, attrName + " return value: " + returnIncreaseStat);
-					return false;
-			}
-		}
+    caap.autoStat = function () {
+        try {
+            if (!config.getItem('AutoStat', false) || !caap.stats['points']['skill']) {
+                return false;
+            }
 
-		con.log(1, "No rules match to increase stats");
-		state.setItem("statsMatch", false);
-		return false;
-	} catch (err) {
-		con.error("ERROR in autoStat: " + err);
-		return false;
-	}
-};
-/*jslint sub: false */
+            if (!state.getItem("statsMatch", true)) {
+                if (state.getItem("autoStatRuleLog", true)) {
+                    con.log(2, "User should possibly change their stats rules");
+                    state.setItem("autoStatRuleLog", false);
+                }
+
+                return false;
+            }
+
+            var atributeSlice = $j("div[style*='keep_top.jpg']", caap.appBodyDiv),
+                startAtt = 0,
+                stopAtt = 4,
+                attrName = '',
+                attribute = '',
+                attrValue = 0,
+                n = 0,
+                returnIncreaseStat = '';
+
+            if (!$u.hasContent(atributeSlice)) {
+                caap.navigateTo('keep');
+                return true;
+            }
+
+            if (config.getItem("AutoStatAdv", false)) {
+                startAtt = 5;
+                stopAtt = 9;
+            }
+
+            for (n = startAtt; n <= stopAtt; n += 1) {
+                attrName = 'Attribute' + n;
+                attribute = config.getItem(attrName, '');
+                if (attribute === '') {
+                    con.log(4, attrName + " is blank: continue");
+                } else {
+                    if (caap.stats['level'] < 10 && (attribute === 'Attack' || attribute === 'Defense' || attribute === 'Health')) {
+                        con.log(1, "Characters below level 10 can not increase Attack, Defense or Health: continue");
+                    } else {
+                        attrValue = config.getItem('AttrValue' + n, 0);
+                        returnIncreaseStat = caap.increaseStat(attribute, attrValue, atributeSlice);
+                        switch (returnIncreaseStat) {
+                            case "Next":
+                                con.log(4, attrName + " : next");
+                                break;
+                            case "Click":
+                                con.log(4, attrName + " : click");
+                                return true;
+                            default:
+                                con.log(4, attrName + " return value: " + returnIncreaseStat);
+                                return false;
+                        }
+                    }
+                }
+            }
+
+            con.log(1, "No rules match to increase stats");
+            state.setItem("statsMatch", false);
+            return false;
+        } catch (err) {
+            con.error("ERROR in autoStat: " + err);
+            return false;
+        }
+    };
+    /*jslint sub: false */
+
+}());
