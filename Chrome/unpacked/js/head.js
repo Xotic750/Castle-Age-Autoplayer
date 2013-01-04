81ff61513ea45f5b5634aa09ffa81b1e310e38c5
Graham Fairweather <xotic750@gmail.com>
2013-01-04 21:02:20 +0100

    Beautified code, ran through jslint and corrected code to latest JS standards.

--- Chrome/unpacked/js/head.js
+++ Chrome/unpacked/js/head.js
@@ -7,70 +7,77 @@
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
-/*jslint white: true, browser: true, devel: true, undef: true, nomen: true, bitwise: true, plusplus: true, immed: true, regexp: true, eqeqeq: true, newcap: true */
-/*global window,escape,jQuery,$j,rison,utility,$u,chrome,CAAP_SCOPE_RUN,self */
-/*jslint maxlen: 512 */
-//////////////////////////////////
-//       External version checking
-//////////////////////////////////
-//var caapjQuery = "1.6.4";
-var caapjQuery = "1.7.1";
-var caapjQueryUI = "1.8.24";
-var caapjQueryDataTables = "1.9.4";
+/*jslint white: true, browser: true, devel: true, undef: true,
+nomen: true, bitwise: true, plusplus: true,
+regexp: true, eqeq: true, newcap: true, forin: false */
+/*global window,escape,jQuery,$j,rison,utility,
+$u,chrome,CAAP_SCOPE_RUN,self,
+schedule,gifting,state,army, general,session,monster,guild_monster */
+/*jslint maxlen: 256 */
+
 //////////////////////////////////
 //       Globals
 //////////////////////////////////
 
-var	caapVersion = "141.0.0",
-		devVersion = "136",
-		hiddenVar = true,
-		caap_timeout = 0,
-		image64 = {},
-		offline = {},
-		profiles = {},
-		session = null,
-		config = null,
-		state = null,
-		css = {},
-		gm = null, 
-		s = null,
-		db = null,
-		sort = {},
-		schedule = null,
-		general = {},
-		monster = {},
-		guild_monster = {},
-		//arena         = {},
-		festival = {},
-		feed = {},
-		battle = {},
-		town = {},
-		spreadsheet = {},
-		gifting = {},
-		army = {},
-		caap = {},
-		con = null,
-		retryDelay = 1000;
+var caapjQuery = "1.7.1",
+	caapjQueryUI = "1.8.24",
+	caapjQueryDataTables = "1.9.4",
+	caapVersion = "141.0.0",
+	devVersion = "136",
+	hiddenVar = true,
+	caap_timeout = 0,
+	image64 = {},
+	offline = {},
+	profiles = {},
+	session = null,
+	config = null,
+	state = null,
+	css = {},
+	gm = null,
+	s = null,
+	db = null,
+	sort = {},
+	schedule = null,
+	general = {},
+	monster = {},
+	guild_monster = {},
+	//arena         = {},
+	festival = {},
+	feed = {},
+	battle = {},
+	town = {},
+	spreadsheet = {},
+	gifting = {},
+	army = {},
+	caap = {},
+	con = {},
+	retryDelay = 1000;
 
 /* This section is formatted to allow Advanced Optimisation by the Closure Compiler */
 /*jslint sub: true */
 String.prototype['stripCaap'] = function() {
-	return this.replace(/caap_/i, '');
+    return this.replace(/caap_/i, '');
 };
+
 String.prototype['numberOnly'] = function() {
-	return parseFloat(this.replace(new RegExp("[^\\d\\.]", "g"), ''));
+    return parseFloat(this.replace(new RegExp("[^\\d\\.]", "g"), ''));
 };
+
 String.prototype['parseTimer'] = function() {
-	var a = [], b = 0, i = 0, l = 0;
-	a = this.split(':');
-	for( i = 0, l = a.length; i < l; i += 1) {
-		b = b * 60 + parseInt(a[i], 10);
-	}
+        var a = [],
+        b = 0,
+        i = 0,
+        l = 0;
 
-	if(isNaN(b)) {
-		b = -1;
-	}
+        a = this.split(':');
+        for( i = 0, l = a.length; i < l; i += 1) {
+                        b = b * 60 + parseInt(a[i], 10);
+        }
 
-	return b;
+        if(isNaN(b)) {
+                        b = -1;
+        }
+
+        return b;
 };
 /*jslint sub: false */
