82bd43bd5e5d9e2f9c20bfa7955527b1fe41d804
Graham Fairweather <xotic750@gmail.com>
2013-01-15 17:27:05 +0100

    Beautify, jslint, nullify

--- Chrome/unpacked/js/caap_guildbattle.js
+++ Chrome/unpacked/js/caap_guildbattle.js
@@ -1,23 +1,39 @@
+/*jslint white: true, browser: true, devel: true, undef: true,
+nomen: true, bitwise: true, plusplus: true,sub: true,
+regexp: true, eqeq: true, newcap: true, forin: false */
+/*global window,escape,jQuery,$j,rison,utility,
+festival,feed,battle,town,
+$u,chrome,CAAP_SCOPE_RUN,self,caap,config,con,gm,hiddenVar,
+schedule,gifting,state,army, general,session,monster,guild_monster */
+/*jslint maxlen: 256 */
+
 /////////////////////////////////////////////////////////////////////
 //                          GUILD BATTLES
 /////////////////////////////////////////////////////////////////////
 
-caap.checkResults_guild_current_battles = function() {
-	try {
-		var tempDiv = $j();
-		tempDiv = $j("img[src*='guild_symbol']");
-		if(tempDiv && tempDiv.length) {
-			tempDiv.each(function() {
-				con.log(5, "name", $j(this).parent().parent().next().text().trim());
-				con.log(5, "button", $j(this).parent().parent().parent().next().find("input[src*='guild_battle_']"));
-			});
-		} else {
-			return false;
-		}
+(function() {
+    "use strict";
 
-		return true;
-	} catch (err) {
-		con.error("ERROR in checkResults_guild_current_battles: " + err);
-		return false;
-	}
-}; 
+    caap.checkResults_guild_current_battles = function() {
+        try {
+            var tempDiv = $j("img[src*='guild_symbol']");
+
+            if (tempDiv && tempDiv.length) {
+                tempDiv.each(function() {
+                    con.log(5, "name", $j(this).parent().parent().next().text().trim());
+                    con.log(5, "button", $j(this).parent().parent().parent().next().find("input[src*='guild_battle_']"));
+                });
+            } else {
+                tempDiv = null;
+                return false;
+            }
+
+            tempDiv = null;
+            return true;
+        } catch (err) {
+            con.error("ERROR in checkResults_guild_current_battles: " + err);
+            return false;
+        }
+    };
+
+}());
